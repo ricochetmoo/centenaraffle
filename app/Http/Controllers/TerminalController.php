aa44fb0311f7aefd5fcf81e97bebeffadf222979
James Barber <james@jamesbarber.tech>
2022-11-16 20:47:08 +0000

    Added charge method

--- app/Http/Controllers/TerminalController.php
+++ app/Http/Controllers/TerminalController.php
@@ -60,4 +60,47 @@ class TerminalController extends Controller
 		$terminal->device_id = $request->data['object']['device_code']['device_id'];
 		$terminal->save();
 	}
+
+	public static function charge($terminalId, $transaction)
+	{
+		$client = new SquareClient
+		([
+			'accessToken' => env('SQUARE_ACCESS_TOKEN'),
+			'environment' => Environment::PRODUCTION,
+		]);
+		
+		$terminal = Terminal::findOrFail($terminalId);
+
+		$amount_money = new \Square\Models\Money();
+		$amount_money->setAmount($transaction->amount);
+		$amount_money->setCurrency('GBP');
+
+		$tip_settings = new \Square\Models\TipSettings();
+		$tip_settings->setAllowTipping(false);
+		$tip_settings->setSeparateTipScreen(false);
+		$tip_settings->setCustomTipField(false);
+
+		$device_options = new \Square\Models\DeviceCheckoutOptions($terminal->device_id);
+		$device_options->setSkipReceiptScreen(true);
+		$device_options->setTipSettings($tip_settings);
+
+		$checkout = new \Square\Models\TerminalCheckout($amount_money, $device_options);
+		$checkout->setReferenceId("CRAF" . $order->id);
+		$checkout->setNote("CentenaRaffle Ticket");
+
+		$body = new \Square\Models\CreateTerminalCheckoutRequest(uniqid(), $checkout);
+
+		$api_response = $client->getTerminalApi()->createTerminalCheckout($body);
+
+		if ($api_response->isSuccess())
+		{
+			$result = $api_response->getResult();
+			return true;
+		}
+		else
+		{
+			$errors = $api_response->getErrors();
+			return response()->json(json_encode($errors), 500);
+		}
+	}
 }
